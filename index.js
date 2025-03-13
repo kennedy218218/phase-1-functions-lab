685a4d294e709731a66782146a43b293de12871c
kennedy <kennedy.mwai@student.moringaschool.com>
2025-03-13 18:09:33 +0300

    completed quiz

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(value){
+    return Math.abs(value - 42);
+}
+
+function distanceFromHqInFeet(value){
+    const blocks = distanceFromHqInBlocks(value);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+const blocksTravelled = Math.abs(destination - start);
+return blocksTravelled * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+
+if(distance <= 400){
+    return 0;
+}
+else if(distance > 400 && distance <= 2000){
+    return(distance - 400) * 0.02;
+}
+else if(distance > 2000 && distance <= 2500){
+    return 25;
+}
+else{
+    return `cannot travel that far`;
+}
+
+
+
+
+
+
+
+
+}
